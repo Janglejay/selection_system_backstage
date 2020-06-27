47bcc3cb27cd7cf433481025aef6dd86fe9ce023
ForU <18644045901@163.com>
2020-06-27 11:25:16 +0800

    init commit

---
+++ src/main/java/com/janglejay/selection_system_backstage/SelectionSystemBackstageApplication.java
@@ -0,0 +1,13 @@
+package com.janglejay.selection_system_backstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectionSystemBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectionSystemBackstageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/janglejay/selection_system_backstage/SelectionSystemBackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janglejay.selection_system_backstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectionSystemBackstageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
